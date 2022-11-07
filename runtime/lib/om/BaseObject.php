4417630133ef884823134f27fec80f4b51c0f7de
Wiebe <wiebe@celerypayroll.com>
2022-11-07 12:03:10 -0400

    refactor: add check for iso format.

--- runtime/lib/om/BaseObject.php
+++ runtime/lib/om/BaseObject.php
@@ -425,4 +425,22 @@ abstract class BaseObject
         }
         throw new PropelException('Call to undefined method: ' . $name);
     }
+
+    /**
+     * Checks if the format is a iso format.
+     *
+     * @param $format
+     * @return bool
+     */
+    protected function isIsoFormat($format)
+    {
+        $arrMatches = ["MM", "DD", "YYYY", "ddd", "HH", "mm"];
+        foreach($arrMatches as $string) {
+            if (strpos($string, $format) !== FALSE) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
