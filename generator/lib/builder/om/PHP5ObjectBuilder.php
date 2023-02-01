5ddb580c6bfdf2cc28bf6524a028e2362bd5a007
Wiebe <wiebe@celerypayroll.com>
2023-02-01 13:10:39 -0400

    refactor: add check for strftime given.
    reference #4

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -981,6 +981,10 @@ abstract class ".$this->getClassname()." extends ".$parentClass." ";
         if (\$this->isIsoFormat(\$format)) {
             throw new PropelException(\"An iso format is used. This should be a date format\");
         }
+        
+        if (strpos(\$format, '%') !== false) {
+            throw new PropelException(\"A strftime format is used. This should be a date format\");
+        }
 
         return \$dt->format(\$format);
         ";
