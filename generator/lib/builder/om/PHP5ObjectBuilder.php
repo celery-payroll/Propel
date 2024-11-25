400be25e8df7bb7b0a9319fee32de7b3e463d07d
FΞLIX <felix@celerypayroll.com>
2024-11-25 09:07:15 -0400

    refactor: change to more compact and readable format
    #16

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -5432,7 +5432,7 @@ abstract class ".$this->getClassname()." extends ".$parentClass." ";
 
         foreach ($table->getColumns() as $col) {
             if (!in_array($col, $autoIncCols, true)) {
-                if ($col->getType() === PropelTypes::TIME || $col->getType() === PropelTypes::TIMESTAMP) {
+                if (in_array($col->getType(), [PropelTypes::TIME, PropelTypes::TIMESTAMP], true)) {
                     $script .= "
         \$copyObj->set" . $col->getPhpName() . "(\$this->get" . $col->getPhpName() . "(null));";
                 } else {
